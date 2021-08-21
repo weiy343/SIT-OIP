577d0b55b70b8f868973f7046c2965ada7e11345
weiy343 <2508496Y@student.gla.ac.uk>
2021-08-21 16:27:09 +0800

    Add initial picamera function

--- server.py
+++ server.py
@@ -1,14 +1,6 @@
 from flask import Flask, request, jsonify, render_template, send_file
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/start")
-def start():
-    print("hello.")
+from picamera import PiCamera
+from time import sleep
 
 def pumping_process():
     print("pumping.")
@@ -29,6 +21,25 @@ def drying_process():
 def sterilizing_process():
     print("sterilizing.")
     pass
+
+def take_picture():
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(5)
+    camera.capture('./images/image.jpg')
+    camera.stop_preview()
+    pass
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/start")
+def start():
+    print("hello.")
+    take_picture()
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
