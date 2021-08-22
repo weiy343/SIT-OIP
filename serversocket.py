d396f65d6a665c90b3fdfcfde3d8c131aaa8fa4c
weiy343 <2508496Y@student.gla.ac.uk>
2021-08-22 22:50:51 +0800

    Add iterations for drying and sterilizing

--- serversocket.py
+++ serversocket.py
@@ -14,50 +14,64 @@ def index():
 @socketio.on("message")
 def startProcess(msg):
     print(msg)
+    iterations = 0
     pumping_process(5)
     washing_process(5)
     draining_process(5)
-    drying_process(5)
-    sterilizing_process(5)
+    drying_process(5, iterations)
+    sterilizing_process(5, iterations)
     isClean = take_picture(5)
     while(not isClean):
-        drying_process(5)
-        sterilizing_process(5)
+        iterations =+ 1
+        drying_process(5, iterations)
+        sterilizing_process(5, iterations)
         isClean = take_picture(5)
         break
     emit("complete")
 
 def pumping_process(time):
     emit("message", {
-        "status": "Pumping",
+        "status": "Pumping water in progress...",
         "time": time
     })
     socketio.sleep(time)
 
 def washing_process(time):
     emit("message", {
-        "status": "Washing",
+        "status": "Washing in progress...",
         "time": time
     })
     socketio.sleep(time)
 
 def draining_process(time):
     emit("message", {
-        "status": "Draining",
+        "status": "Draining in progress...",
         "time": time
     })
     socketio.sleep(time)
 
-def drying_process(time):
+def drying_process(time, retry):
+
+    status = "Drying in progress..."
+
+    if retry > 0:
+        status = f"Drying ({retry}) in progress..."
+
     emit("message", {
-        "status": "Drying",
-        "time": time
+        "status": status,
+        "time": time,
     })
     socketio.sleep(time)
 
-def sterilizing_process(time):
+def sterilizing_process(time, retry):
+
+    status = "Sterilizing in progress..."
+
+    if retry > 0:
+        status = f"Sterilizing ({retry}) in progress..."
+
     emit("message", {
-        "status": "Sterilizing",
+        "status": status,
         "time": time
     })
     socketio.sleep(time)
